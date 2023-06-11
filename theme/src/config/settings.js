15bf34f0cc64c7d452d4a0eb9aa49c76540a1e77
HeisenBerg? <edwin_uestc@163.com>
2023-06-11 15:21:22 +0800

    Update settings.js

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `100+ side projects challenges before 40| From idea to action`,
+	description: `one day I realized there is 1000+ github repo under my account, and got thousands of stars there.but I dont even get a website to write down what trigger my idead and what I did`,
+	url: `http://projects.shopconna.com/`, // No trailing slash!
+	name: `100+ projects challenges`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
